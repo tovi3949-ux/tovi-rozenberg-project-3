dd3f62cded384b438a4adbfc2cf8ec58caaecd91
Tovi Rozenberg <tovi3949@gmail.com>
2025-11-30 01:50:16 +0200

    node2

--- NodeApi/get_project.js
+++ NodeApi/get_project.js
@@ -1,6 +1,7 @@
 import renderApi from '@api/render-api';
 
 renderApi.auth('rnd_ckPGUIDiyXNLMMVD3UDdbovzAqKK');
-renderApi.listServices({environmentId: '', includePreviews: 'true', limit: '20'})
+
+renderApi.listServices({ includePreviews: true, limit: 20 })
   .then(({ data }) => console.log(data))
   .catch(err => console.error(err));
